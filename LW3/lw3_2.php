d68994f8eed2dad177b410b38e75ff898f0f2530
GeneralK1 <103638653+GeneralK1@users.noreply.github.com>
2022-10-11 13:39:31 +0300

    Update lw3_2.php

--- LW3/lw3_2.php
+++ LW3/lw3_2.php
@@ -1,23 +1,30 @@
 <?php
-header('Content-Type: text/plain');
 
-function CheckIdentifier($text){
-   if (($text === null) or ($text === ''))
-   {
-      echo 'No Identifier cant be empty';
-      return;
-   }
-   if(!ctype_alpha($text[0])){
-      echo 'No Identifier cant start with a symbol', $text[0];
-      return;
-   }
-   for($i = 0; $i < strlen($text); $i++){
-      if(!ctype_alpha($text[$i]) && !is_numeric($text[$i])){
-         echo 'No Symbol ', $text[$i], ' is unavailable symbol in Identifier';
-	 return;
-      }
-   }
-   echo 'Yes';
+$_SERVER['QUERY_STRING'];
+$tempString = $_GET['identifier'];
+$flag = true;
+
+if ((is_numeric($tempString[0])) or (empty($tempString)))
+{
+	echo 'no';
+	$flag = false;
+} else
+{
+	while (!(empty($tempString[0])))
+	{
+		if ((is_numeric($tempString[0])) or (ctype_alpha($tempString[0])))
+		{
+			$tempString = substr($tempString, 1);
+		} else 
+		{
+			echo 'no';
+			$tempString = '';
+			$flag = false;
+		}
+	} 
 }
-
-echo CheckIdentifier($_GET["identifier"]);
+if ($flag) 
+{
+	echo 'yes';
+}
+?>
